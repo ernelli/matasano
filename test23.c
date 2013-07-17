0291036a9d12ff7fc53a15bfe6afa6581a3a603d
Mattias Ernelli <mattias@mattias-1000H.(none)>
2013-07-17 22:27:34 +0200

    Update

--- test23.c
+++ test23.c
@@ -6,13 +6,21 @@ int main(int argc, char *argv[]) {
   int i, wait1, wait2, seed, start;
   unsigned int val;
 
-  MT_initialize_generator(1337);
+  random_bytes((unsigned char *)&seed, sizeof(seed));
 
-  for(i = 0; i < 10; i++) {
-    val =MT_extract_number();
-    printf("revers: %u\n", MT_reverse_number(val));
+  MT_initialize_generator(seed);
+
+  struct MT_generator clone;
+
+  clone.index = 0;
+
+  for(i = 0; i < 624; i++) {
+    clone.state[i] = MT_untemper_number(MT_extract_number());
   }
   
+  for(i = 0; i < 10; i++)  {
+    printf("original: %u clone %u\n", MT_extract_number(), extract_number(&clone));
+  }
 
   return 0;
 }
--- tools.h
+++ tools.h
@@ -22,7 +22,21 @@ void aes_ecb_decrypt(unsigned char *block, int len, unsigned char *key, int key_
 void aes_cbc_encrypt(unsigned char *block, int len, unsigned char *key, int key_len, unsigned char *iv);
 void aes_cbc_decrypt(unsigned char *block, int len, unsigned char *key, int key_len, unsigned char *iv);
 
+void mt19937_encrypt(unsigned char *data, int len, unsigned short key);
+void mt19937_decrypt(unsigned char *data, int len, unsigned short key);
+
 void seed_random(const unsigned char *seed, int len);
 void random_bytes(unsigned char *data, int len);
 int detect_ecb(unsigned char *indata, int len, int blocklen);
+
+struct MT_generator {
+  unsigned int state[624];
+  int index;
+} MT;
+
+void initialize_generator(struct MT_generator *gen, unsigned int seed);
+void MT_initialize_generator(unsigned int seed);
+unsigned int extract_number(struct MT_generator *gen);
+unsigned int MT_extract_number();
+
 #endif
